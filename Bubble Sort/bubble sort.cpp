a725e031f284c3740a7d6ae697183dafb3bf6163
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-01-05 23:36:19 +0600

    Update bubble sort.cpp

--- Bubble Sort/bubble sort.cpp
+++ Bubble Sort/bubble sort.cpp
@@ -1,7 +1,7 @@
 for(int i=0; i<n-1; i++){
     for(int j=0; j<n-i-1; j++){
         if(arr[j] > arr[j+1]){
-            swap(arr[j],arr[j+1]);
+            swap(arr[j], arr[j+1]);
                 swaps++;
             }
         }
