8d55bf90b0ed7d896089e3127f7152fd612b90e1
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-01-06 12:14:46 +0600

    Create binary search.cpp

---
+++ Binary Search/binary search.cpp
@@ -0,0 +1,46 @@
+void binary_search(int n, int target, int arr[]){
+    int left = 0;
+    int right = arr.size() - 1;                            //-1 to access the last element where index started from 0
+
+    while(left <= right){
+        int mid = (left + right) / 2;
+
+        // Check if the target is present at the middle
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        // If target is greater, ignore the left half
+        if(arr[mid] < target){
+            left = mid + 1;
+        }
+        // If target is smaller, ignore the right half
+        else{
+            right = mid - 1;
+        }
+    }
+
+    // If we reach here, then the element was not present
+    return -1;
+}
+
+/*
+
+Best case = O(1)
+y
+|
+|__________
+|          x
+Worst case = O(logn)
+Average case = O(logn)
+y
+|
+|     
+|     ____
+|   /     ----
+|  /          
+| /
+|/
+------------------x
+
+*/
