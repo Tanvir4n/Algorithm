2a9e897715ba928eb918714b78de8489e258966c
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-01-06 23:49:48 +0600

    Create selection sort.cpp

---
+++ Selection Sort/selection sort.cpp
@@ -0,0 +1,21 @@
+void selection_sort(vector<int>&arr){
+    
+    int n=arr.size();
+    int swaps=0;
+    
+    for(int i=0; i<n-1; i++){                   // Find the minimum element in the unsorted part of the array
+        int minIndex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);            // Swap the found minimum element with the first element of the unsorted part
+        swaps++;
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Number of swaps needed: "<<swaps<<endl;
+}
