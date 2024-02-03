d1ec35c6e07818641959589fd8ea10aa28e8b6cc
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-02-03 14:11:58 +0600

    Update selection sort.cpp

--- Selection Sort/selection sort.cpp
+++ Selection Sort/selection sort.cpp
@@ -22,6 +22,13 @@ void selection_sort(vector<int>&arr){
 
 /*
 
-time complexity O(n^2)
+time complexity 
 
+Best-case: O(n2), best case occurs when the array is already sorted. (where n is the number of integers in an array)
+
+Average-case: O(n2), the average case arises when the elements of the array are in a disordered or random order, without a clear ascending or descending pattern.
+
+Worst-case: O(n2), The worst-case scenario arises when we need to sort an array in ascending order, but the array is initially in descending order.
+
+Space Complexity: O(1), as no extra space is required for the Selection sort algorithm
 */
