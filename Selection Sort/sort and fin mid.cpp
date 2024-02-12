4294d412fd4bf0941e2c2b83c6f29a937576c37e
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-02-12 23:17:51 +0600

    Create sort and fin mid.cpp

---
+++ Selection Sort/sort and fin mid.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort(vector<int>& arr, int n){
+    for(int i=0; i<n-1; i++){
+        int minIndex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    if(n<=4 || n>100){
+        return 1;
+    }
+
+    vector<int> arr(n);
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    selection_sort(arr, n);
+
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    int mid;
+
+    if(n%2 == 0){
+        mid = (arr[n/2 - 1] + arr[n/2]) / 2;
+        
+    }
+    else{
+        mid = arr[n/2];
+    }
+
+    cout<<mid<<endl;
+
+    return 0;
+}
