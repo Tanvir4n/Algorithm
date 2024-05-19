33063090c7df7b6e836bd12550143ce2dad8caab
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-05-19 21:41:43 +0600

    Create 2D sorting.cpp

---
+++ Sorting/2D sorting.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+void FastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solution(){
+
+   int t;
+   cin>>t;
+   vector<pair<int, int>>v(t);
+   for(int i=0; i<t; i++){
+       cin>>v[i].first>>v[i].second;
+   }
+   sort(v.begin(), v.end());
+   for(int i=0; i<t; i++){
+       cout<<v[i].first<<" "<<v[i].second<<endl;
+   }
+}
+
+int32_t main(){
+    FastIO();
+    
+    solution();
+
+}
